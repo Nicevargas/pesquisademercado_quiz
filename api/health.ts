b5fb32d28e48b3a2fae09e0b08a8725da0670a92
Nice Vargas <eunicelvargas@gmail.com>
2026-07-28 12:00:45 -0300

    Change import from supabaseServer.ts to supabaseServer.js

--- api/health.ts
+++ api/health.ts
@@ -1,4 +1,4 @@
-import { getSupabaseClient } from '../src/lib/supabaseServer.ts';
+import { getSupabaseClient } from '../src/lib/supabaseServer.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
